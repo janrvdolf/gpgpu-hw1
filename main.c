81d5ace2301c7dd112818de3eb3191696f876175
Jan Rudolf <rudolja1@fel.cvut.cz>
2020-10-16 20:37:27 +0200

    Starting and ending threads.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <pthread.h>
 
-// stage 1 settings - dimension of randomly generated matrices
+// stage 1 settings
 #define MATRIX_COUNT 10
 #define MATRIX_DIM 2
 
@@ -16,7 +16,11 @@ typedef int buffer_item;
 
 buffer_item buffer[STAGE2_BUFFER_SIZE];
 
+pthread_attr_t  attr;
+
 /* stage 1 sync structures */
+pthread_mutex_t mutex;
+
 
 /* stage 2 sync structures */
 
@@ -24,7 +28,83 @@ buffer_item buffer[STAGE2_BUFFER_SIZE];
 
 /* stage 4 structures */
 
+
+void *stage_1(void *attr) {
+
+    pthread_exit(NULL);
+}
+
+void *stage_2_master(void *attr) {
+
+    pthread_exit(NULL);
+}
+
+void *stage_2_worker(void *attr) {
+
+    pthread_exit(NULL);
+}
+
+void *stage_3 (void *attr) {
+
+    pthread_exit(NULL);
+}
+
+void *stage_4(void *attr) {
+
+    pthread_exit(NULL);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    // init
+    pthread_t thr_stage_1;
+    pthread_t thr_stage_2_master;
+    pthread_t thr_stage_2_workers[STAGE2_WORKERS_COUNT];
+    pthread_t thr_stage_3;
+    pthread_t thr_stage_4;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    pthread_mutex_init(&mutex, NULL);
+
+    // create threads
+    int is_thr_stage_1 = pthread_create(&thr_stage_1, &attr, (void * (*) (void *)) stage_1, NULL);
+    if (!is_thr_stage_1) {
+        printf("Error: stage 1 thread isn't created\n");
+    }
+
+    int is_thr_stage_2_master = pthread_create(&thr_stage_2_master, &attr, (void * (*) (void *)) stage_2_master, NULL);
+    if (!is_thr_stage_2_master) {
+        printf("Error: stage 2 master thread isn't created\n");
+    }
+
+    for (int i = 0; i < STAGE2_WORKERS_COUNT; i++) {
+        int is_thr_stage_2_worker = pthread_create(&thr_stage_2_master, &attr, (void * (*) (void *)) stage_2_master, NULL);
+        if (!is_thr_stage_2_worker) {
+            printf("Error: stage 2 thread num %d isn't created\n", i);
+        }
+    }
+
+    int is_thr_stage_3 = pthread_create(&thr_stage_3, &attr, (void * (*) (void *)) stage_3, NULL);
+    if (!is_thr_stage_3) {
+        printf("Error: stage 3 thread isn't created\n");
+    }
+
+    int is_thr_stage_4 = pthread_create(&thr_stage_4, &attr, (void * (*) (void *)) stage_4, NULL);
+    if (!is_thr_stage_4) {
+        printf("Error: stage 4 thread isn't created\n");
+    }
+
+    // exit - threads join, destroy/clean up
+    pthread_join(thr_stage_1, NULL);
+    pthread_join(thr_stage_2_master, NULL);
+    for (int i = 0; i < STAGE2_WORKERS_COUNT; i++) {
+        pthread_join(thr_stage_2_workers[i], NULL);
+    }
+    pthread_join(thr_stage_3, NULL);
+    pthread_join(thr_stage_4, NULL);
+
+    pthread_attr_destroy(&attr);
+
     return 0;
 }
